651e68c890413db4a9ce58e06d33316a4beefa3a
Ameer_105 <ameerdamilola@gmail.com>
2022-06-03 19:04:21 +0100

    still working on the download button
    Download button is opening the generated wallpaper on another tab
    
    Progress!!!!!😹

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ image_array = [
   }
 
 
+  /*let btnDownload = document.querySelector('button');
+let img = document.querySelector('img');
+// Must use FileSaver.js 2.0.2 because 2.0.3 has issues.
+btnDownload.addEventListener('click', () => {
+    let imagePath = img.getAttribute('src');
+    let fileName = getFileName(imagePath);
+    saveAs(imagePath, fileName);
+});
+function getFileName(str) {
+    return str.substring(str.lastIndexOf('/') + 1)
+}*/
+
