d7b2a7b7ac037c1e1f63a9ce4f0e34dbf5b14c20
Ameer_105 <ameerdamilola@gmail.com>
2022-06-05 18:35:01 +0100

    Removed unwanted JavaScript comments

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 image_array = [
     '1.jpg',
     '2.jpg',
-    '3.jpg'
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg',
+    '9.jpg',
+    '10.jpg'
+
   ]
   
   function get_random_image(){
@@ -9,17 +17,4 @@ image_array = [
     selected_image = image_array[random_index]
     document.getElementById('wallpaper').src = `./img/${selected_image}`
   }
-
-
-  /*let btnDownload = document.querySelector('button');
-let img = document.querySelector('img');
-// Must use FileSaver.js 2.0.2 because 2.0.3 has issues.
-btnDownload.addEventListener('click', () => {
-    let imagePath = img.getAttribute('src');
-    let fileName = getFileName(imagePath);
-    saveAs(imagePath, fileName);
-});
-function getFileName(str) {
-    return str.substring(str.lastIndexOf('/') + 1)
-}*/
 
