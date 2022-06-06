e7e272e3d4397837142ed987fac8d3f6ba4c4bcb
Ameer_105 <ameerdamilola@gmail.com>
2022-06-06 22:33:34 +0100

    linking categories to their various pages

---
+++ neon/script.js
@@ -0,0 +1,20 @@
+image_array = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg',
+    '9.jpg',
+    '10.jpg'
+
+  ]
+  
+  function get_random_image(){
+    random_index = Math.floor(Math.random() * image_array.length);
+    selected_image = image_array[random_index]
+    document.getElementById('wallpaper').src = `./img/${selected_image}`
+  }
+
