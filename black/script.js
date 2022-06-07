722a4f659d1fa83a6521cc6105cabc4719daa952
Ameer_105 <ameerdamilola@gmail.com>
2022-06-07 07:48:03 +0100

    added 27 wallpapers to black

--- black/script.js
+++ black/script.js
@@ -1,20 +1,36 @@
 image_array = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg',
-    '4.jpg',
-    '5.jpg',
-    '6.jpg',
-    '7.jpg',
-    '8.jpg',
-    '9.jpg',
-    '10.jpg'
-
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jpg',
+  '5.jpg',
+  '6.jpg',
+  '7.jpg',
+  '8.jpg',
+  '9.jpg',
+  '10.jpg',
+  '11.jpg',
+  '12.jpg',
+  '13.jpg',
+  '14.jpg',
+  '15.jpg',
+  '16.jpg',
+  '17.jpg',
+  '18.jpg',
+  '19.jpg',
+  '20.jpg',
+  '21.jpg',
+  '22.jpg',
+  '23.jpg',
+  '24.jpg',
+  '25.jpg',
+  '26.jpg',
+  '27.jpg'
   ]
   
   function get_random_image(){
     random_index = Math.floor(Math.random() * image_array.length);
     selected_image = image_array[random_index]
-    document.getElementById('wallpaper').src = `./img/${selected_image}`
+    document.getElementById('wallpaper').src = `./black/img/${selected_image}`
   }
 
