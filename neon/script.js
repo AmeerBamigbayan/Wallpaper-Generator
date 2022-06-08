3136fdcd0ab1e060e05132539ac22fb5b1f7c984
Ameer_105 <ameerdamilola@gmail.com>
2022-06-08 01:54:16 +0100

    Added no-repeat feature

--- neon/script.js
+++ neon/script.js
@@ -41,5 +41,49 @@ image_array=[
     random_index = Math.floor(Math.random() * image_array.length);
     selected_image = image_array[random_index]
     document.getElementById('wallpaper').src = `./neon/img/${selected_image}`
-  }
+    image_array.splice(random_index, 1);
 
+    if(image_array.length == 0){
+      image_array = [
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+        '4.jpg',
+        '5.jpg',
+        '6.jpg',
+        '7.jpg',
+        '8.jpg',
+        '9.jpg',
+        '10.jpg',
+        '11.jpg',
+        '12.jpg',
+        '13.jpg',
+        '14.jpg',
+        '15.jpg',
+        '16.jpg',
+        '17.jpg',
+        '18.jpg',
+        '19.jpg',
+        '20.jpg',
+        '21.jpg',
+        '22.jpg',
+        '23.jpg',
+        '24.jpg',
+        '25.jpg',
+        '26.jpg',
+        '27.jpg',
+        '28.jpg',
+        '29.jpg',
+        '30.jpg',
+        '31.jpg',
+        '32.jpg',
+        '33.jpg',
+        '34.jpg',
+        '35.jpg',
+        '36.jpg'
+      ]
+    }
+
+
+  }
+   
